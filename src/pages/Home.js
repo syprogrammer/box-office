d5c39d579f4a1fe0f5799f80f6f79e63a1711f57
skayinself <newstoday1950@gmail.com>
2022-05-18 17:19:03 +0530

    created config file for fetchind data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = React.useState('');
+  const [results, setResults] = React.useState(null);
+
   const onInputChange =(ev) =>{
     setInput(ev.target.value);
   }
@@ -14,16 +17,35 @@ const Home = () => {
 }
 
   const onSearch = () => {
-    // 
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(resolve => resolve.json()).then(result => {
-      console.log(result);
-    });
+    
+    apiGet(`/search/shows?q=${input}`).then(result => {
+    setResults(result)
+  });
+
+}
+  const renderResults = () => {
+    if (results && results.length === 0) {
+        return <div>No results try different names</div>
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {
+            results.map((item) => (
+              <div key={item.show.id}>{item.show.name}</div>
+            ))
+          }
+        </div>
+      );
+    }
+    return null;
   }
   return (
     <>
       <MainPageLayout>
         <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
        <button type='button' onClick={onSearch}>search</button>
+       {renderResults()}
       </MainPageLayout>
     </>
   )
