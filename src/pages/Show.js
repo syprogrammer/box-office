420015d4623c11e05ef5b9fb72f3120490ff35e6
skayinself <newstoday1950@gmail.com>
2022-05-20 17:33:49 +0530

    added loading and error message

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,17 +4,45 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [error,setError] =useState(null)
+
   useEffect(() => {
+    let isMounted = true;
+
+
     apiGet(`/shows/${id}?embed[]=season&embed[]=cast`)
       .then(results => {
-       setShow(results);
-    })
+        
+          if (isMounted) {
+              setShow(results);
+              setIsLoading(false);
+          }
+         
+   
+       
+      }).catch(err => {
+        if (isMounted) {
+           setError(err.message);
+           setIsLoading(false);
+        }
+       
+      })
+    return () => {
+      isMounted = false;
+    }
   }, [id])
    
   
 
-console.log("show",show)
-
+  console.log("show", show)
+  if (isLoading) {
+    return <div>Data is being loaded</div>
+  }
+  if (error) {
+  return <div>error occured</div>
+}
   return (
       <>
           <h1>This is show page</h1>
