dbbd34a46c7622770b1d1cc6655a8a6b902b1bbc
skayinself <newstoday1950@gmail.com>
2022-05-26 17:47:35 +0530

    Updates

---
+++ precache-manifest.3791b172d71fb8b4abfe4a8752c90ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca91f03010759dd03b99846f0b66c34a",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "4128b173d2c0932df762",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "26cf32c4db9d305e4054",
+    "url": "/box-office/static/js/2.7366c5da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.7366c5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4128b173d2c0932df762",
+    "url": "/box-office/static/js/main.f0c4d88f.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
